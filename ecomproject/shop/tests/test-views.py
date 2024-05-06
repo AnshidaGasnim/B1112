27f45521086f54d01e9ae4d8a648d072cd51c17b
AnshidaGasnim <anshidagasnim@gmail.com>
2024-05-06 18:33:47 +0300

    added

--- ecomproject/shop/tests/test-views.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase, Client
-from django.urls import reverse, resolve
-
-class TestViews(TestCase):
-    def test_home(self):
-        client=Client()
-        response=client.get(reverse('home'))
-        self.assertEquals(response.status_code,200)
-        self.assertTemplateUsed(response,'home.html')
